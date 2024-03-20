ad6aca89dd4cbe0ddace99124a9e6ee02340431a
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-20 00:20:51 -0400

    not sure what happened

---
+++ fields/colorPicker.tsx
@@ -0,0 +1,59 @@
+import type { BasicFormField, FormFieldStoredValue } from "@keystatic/core";
+import { FieldPrimitive } from "@keystar/ui/field";
+
+function parseAsNormalField(value: FormFieldStoredValue) {
+  if (value === undefined) {
+    return "";
+  }
+  if (typeof value !== "string") {
+    throw new Error("Must be a string");
+  }
+  return value;
+}
+
+export function colorPicker({
+  label,
+  defaultValue,
+  description,
+}: {
+  label: string;
+  defaultValue?: string;
+  description?: string;
+}): BasicFormField<string> {
+  return {
+    kind: "form",
+    formKind: undefined,
+    label,
+    Input(props) {
+      return (
+        <FieldPrimitive description={description} label={label}>
+          <input
+            type="color"
+            autoFocus={props.autoFocus}
+            value={props.value}
+            onChange={(e) => {
+              props.onChange(e.target.value);
+            }}
+          />
+        </FieldPrimitive>
+      );
+    },
+    defaultValue() {
+      return defaultValue || "";
+    },
+    parse(value) {
+      return parseAsNormalField(value);
+    },
+    serialize(value) {
+      return { value: value === "" ? undefined : value };
+    },
+    validate(value) {
+      return value;
+    },
+    reader: {
+      parse(value) {
+        return parseAsNormalField(value);
+      },
+    },
+  };
+}
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,25 +1,28 @@
-import { config, fields, collection } from '@keystatic/core';
-
+import { config, fields, collection } from "@keystatic/core";
+import { colorPicker } from "./fields/colorPicker";
 export default config({
   storage: {
-    kind: 'local',
+    kind: "local",
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.document({
-          label: 'Content',
-          formatting: true,
-          dividers: true,
-          links: true,
-          images: {
-            directory: 'src/assets/images/posts',
-            publicPath: '../../assets/images/posts/',
+        title: fields.slug({ name: { label: "Title" } }),
+        accentColor: colorPicker({
+          label: "Accent Color",
+          defaultValue: "#000000",
+        }),
+        content: fields.markdoc({
+          label: "Content",
+          options: {
+            image: {
+              directory: "src/assets/images/posts",
+              publicPath: "../../assets/images/posts/",
+            },
           },
         }),
       },
